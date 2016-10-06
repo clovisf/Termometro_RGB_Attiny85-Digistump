8dbf04a9c16ce4c9bf3365b0204959d53f019ef5
clovisf <clovisf@gmail.com>
2016-10-05 23:59:11 -0300

    Adicionados arquivos. Código melhorado
    Adicionados arquivos (fotos, tabelas). Código do Termômetro RGB foi
    melhorado.

--- Termometro_RGB_Attiny85_r00_05102016/Termometro_RGB_Attiny85_r00_05102016.ino
+++ Termometro_RGB_Attiny85_r00_05102016/Termometro_RGB_Attiny85_r00_05102016.ino
@@ -15,50 +15,43 @@
    
  Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);  
    
- int delayval = 333; // delay  
+ int delayval = 500; // delay  
    
  void setup() {  
    pixels.begin(); // This initializes the NeoPixel library.  
-   pixels.setBrightness(80);
+   pixels.setBrightness(60);
  }  
    
  void loop() {  
    
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.  
    
-  for(int i=0;i<NUMPIXELS;i++){  
+  //for(int i=0;i<NUMPIXELS;i++){  
 
      sensorValue = (analogRead(sensorPin)- 194.3197) / 13.2447;
       
-     if (sensorValue < 8){ //from total blue to green
-      Blue = map(sensorValue, 0, 8, 255, 0);
-      Green = map(sensorValue, 0, 8, 0, 255);
+     if (sensorValue < 20){ //from total blue to green
+      Blue = map(sensorValue, 0, 19.99, 255, 0);
+      Green = map(sensorValue, 0, 19.99, 0, 255);
       Red = 0;
      }
-     if ((sensorValue >= 8) &&(sensorValue < 30)) { // from green to yellow
+     if ((sensorValue >= 20) &&(sensorValue < 30)) { // from green to yellow
      Green = 255;
-     Red = map(sensorValue, 8, 30, 0, 255); 
+     Red = map(sensorValue, 20, 29.99, 0, 255); 
      Blue = 0;
      }
      if (sensorValue >=30){ // yellow and up to red
       Red = 255;
       Green = map(sensorValue, 30, 45, 255, 0);
-      Blue = 0;
-      
+      Blue = 0;      
      }
-     
-     //sensorValue = map(sensorValue, 0, 1023, 0, 255);     
+            
    
    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255  
-   pixels.setPixelColor(i, pixels.Color(Red,Green,Blue)); // Moderately bright green color.  
+   pixels.setPixelColor(0, pixels.Color(Red,Green,Blue)); // Moderately bright green color.  
    pixels.show(); // This sends the updated pixel color to the hardware.  
    delay(delayval); // Delay for a period of time (in milliseconds).  
-  /* pixels.setPixelColor(i, pixels.Color(0,sensorValue,0)); // Moderately bright green color.  
-   pixels.show(); // This sends the updated pixel color to the hardware.  
-   delay(delayval); // Delay for a period of time (in milliseconds).  
-   pixels.setPixelColor(i, pixels.Color(0,0,sensorValue)); // Moderately bright green color.  
-   pixels.show(); // This sends the updated pixel color to the hardware.  
-   delay(delayval); // Delay for a period of time (in milliseconds).  */
+
    
-  }  
+  //}  
  }  
